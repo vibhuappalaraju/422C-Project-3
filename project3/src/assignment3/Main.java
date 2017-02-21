2b518dd49fe55e3bf203b739c2826ee87860e205
Vibhu Appalaraju <vibhuappalaraju@gmail.com>
2017-02-21 17:58:28 -0600

    updated bfs

--- project3/src/assignment3/Main.java
+++ project3/src/assignment3/Main.java
@@ -9,203 +9,180 @@
  * <Student2 5-digit Unique No.>
  * Slip days used: <0>
  * Git URL:
- * Spring 2017
+ * Fall 2016
  */
-//This is a test change
 
-package assignment3;
+package project3;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    static ArrayList<String> wordladderBFS = new ArrayList<String>();
-	static ArrayList<String> wordladderDFS = new ArrayList<String>();
-    
+	static ArrayList<String> wordladder = new ArrayList<String>();
+	// static variables and constants only here.
+
 	public static void main(String[] args) throws Exception {
-		
-		Scanner kb;	// input Scanner for commands
-		PrintStream ps;	// output file
-		// If arguments are specified, read/write from/to files instead of Std IO.
+
+		Scanner kb; // input Scanner for commands
+		PrintStream ps; // output file
+		// If arguments are specified, read/write from/to files instead of Std
+		// IO.
 		if (args.length != 0) {
 			kb = new Scanner(new File(args[0]));
 			ps = new PrintStream(new File(args[1]));
-			System.setOut(ps);			// redirect output to ps
+			System.setOut(ps); // redirect output to ps
 		} else {
 			kb = new Scanner(System.in);// default from Stdin
-			ps = System.out;			// default to Stdout
+			ps = System.out; // default to Stdout
 		}
-		ArrayList<String> userInput = new ArrayList<String>();
-		userInput = parse(kb);
-        
-        while(!userInput.contains("/quit")){
-        	initialize();
-        	while(userInput.size() < 2){
-        		userInput = parse(kb);
-        	}
-        	wordladderBFS = getWordLadderBFS(userInput.get(0),userInput.get(1));
-        	wordladderDFS = getWordLadderDFS(userInput.get(0),userInput.get(1));
-        	printLadder(wordladderBFS);
-        	System.out.println();
-        	printLadder(wordladderDFS);
-        	System.out.println();
-        	userInput.clear();
-        }
+
+		wordladder = parse(kb);
+		if (wordladder == null) {
+			return;
+		}
+
+		initialize();
+		printLadder(getWordLadderDFS(wordladder.get(0), wordladder.get(1)));
+		System.out.println("");
+		printLadder(getWordLadderBFS(wordladder.get(0), wordladder.get(1)));
+
+		// TODO methods to read in words, output ladder
 	}
-	
+
 	public static void initialize() {
+
 		// initialize your static variables or constants here.
-		// We will call this method before running our JUNIT tests.  So call it 
+		// We will call this method before running our JUNIT tests. So call it
 		// only once at the start of main.
 	}
-	
+
 	/**
-	 * @param keyboard Scanner connected to System.in
-	 * @return ArrayList of 2 Strings containing start word and end word. 
-	 * If command is /quit, return empty ArrayList. 
+	 * @param keyboard
+	 *            Scanner connected to System.in
+	 * @return ArrayList of 2 Strings containing start word and end word. If
+	 *         command is /quit, return empty ArrayList.
 	 */
 	public static ArrayList<String> parse(Scanner keyboard) {
-		ArrayList<String> checklist = new ArrayList<String>();
-		checklist.add(keyboard.next());
-		if(checklist.contains("/quit")){
-			System.out.print("h");
-			System.exit(0);
+		// TO DO
+		ArrayList<String> kb = new ArrayList<String>();
+		kb.add(keyboard.next().toLowerCase());
+
+		if (kb.contains("/quit")) {
+			ArrayList<String> emptylist = new ArrayList<String>();
+			return emptylist;
+		} else {
+			kb.add(keyboard.next().toLowerCase());
+			if (kb.contains("/quit")) {
+				ArrayList<String> emptylist = new ArrayList<String>();
+				return emptylist;
+			}
+
 		}
-		else{
-			checklist.add(keyboard.next());
-			if(checklist.contains("/quit")){
-				System.out.print("i");
-				System.exit(0);
+		return kb;
+	}
+/**
+ * 
+ * @param start 
+ * @param end
+ * @return
+ */
+	public static ArrayList<String> getWordLadderDFS(String start, String end) {
+		Set<String> Dictionary = makeDictionary();
+
+
+		ArrayList<String> printlist = new ArrayList<String>();
+		printlist.add(start);
+		printlist = DFS(start, end, printlist, Dictionary);
+		if(printlist == null){
+			printlist = new ArrayList<String>();
+			printlist.add(start.toLowerCase());
+		printlist.add(end.toLowerCase());}
+		return printlist;
+		
+	}
+	
+/**
+ * 
+ * @param start
+ * @param end
+ * @return
+ */
+	public static ArrayList<String> getWordLadderBFS(String start, String end) {
+
+		Set<String> Dictionary = makeDictionary();
+		Queue<Node> words = new LinkedList<Node>();
+		Node first = new Node();
+		first.storeword = start;
+		words.add(first);
+		int wordlength = start.length();
+		boolean queuechecker = words.isEmpty();
+		int x = 0;
+		int y = 0;
+
+		while (!queuechecker) {
+			Node addtoqueue = new Node();
+			addtoqueue = words.remove();
+			String freshword = addtoqueue.storeword;
+
+			addtoqueue.storeladder.add(freshword);
+
+			if (freshword.equals(end)) {
+
+				
+
+				// return the ladder here instead
+
+				return addtoqueue.storeladder;
+
 			}
 			
-		}
-		return checklist;
-		
-		
-		
-	}
-	
-	public static ArrayList<String> getWordLadderDFS(String start, String end) {
-		
-		// Returned list should be ordered start to end.  Include start and end.
-		// Return empty list if no ladder.
-		// TODO some code
-		Set<String> dict = makeDictionary();
-        if (end.equals(start)){
-            System.out.print("no word ladder can be found between "+start+" and "+end);
-            System.exit(0);
-        }
-        
-        ArrayList<String> printlist = new ArrayList<String>();
-        printlist.add(start);
-        printlist=DFS(start,end,printlist,dict);
-        
-        return printlist;
-		// TODO more code
-		
-		
-		
-		 // replace this line later with real return
-	}
-	
-    public static ArrayList<String> getWordLadderBFS(String start, String end) {
-    	
-    	Queue<Node> Queue = new LinkedList<Node>();
-    	ArrayList<String> wordLadder = new ArrayList<String>();
-		Set<String> dict = makeDictionary();
-		Set<String> visitedWords = new HashSet<String>();
-		
-		Node head = new Node(start);
-		
-		Queue.add(head);				//Add starting word to the Queue
-		visitedWords.add(head.word);	//Add to visited set so no word repeats
-		head.parentNode = null;			
-		
-		while(!Queue.isEmpty()){	
-			head = Queue.poll();		//Remove top element from the queue
 
-			if(head.word.equals(end)){	//End search if we found the end word
-				break;
-			}
-				
-			head.neighbors = allNeighbors(head.word, dict); //find all words in dictionary with one letter difference
-				
-			for(Node n : head.neighbors){ 				//Iterate through all "neighbors"
-				if(!visitedWords.contains(n.word)){
-					visitedWords.add(n.word);
-					n.parentNode = head;			//Make the parent node head -> for backtracing
-					Queue.add(n);					
+			StringBuilder wordusing = new StringBuilder(freshword);
+
+			Dictionary.remove(wordusing.toString().toUpperCase());
+			while (x < wordlength) {
+				while (y < 26) {
+					wordusing.setCharAt(x, (char) ('a' + y));
+					if (Dictionary.contains(wordusing.toString().toUpperCase())) {
+						Node addword = new Node();
+						addword.storeword = wordusing.toString();
+						addword.storeladder = new ArrayList<String>(addtoqueue.storeladder);
+						words.add(addword);
+						Dictionary.remove(wordusing.toString());
+
+					}
+
+					wordusing = new StringBuilder(freshword);
+					y++;
+
 				}
+
+				x++;
+				y = 0;
+
 			}
 
+			x = 0;
+			queuechecker = words.isEmpty();
+
 		}
-		
-		
-		
-		if(head.word.equals(end)){
-			//Uses parent pointers to add path to word ladder array list
-			while(head != null){
-				wordLadder.add(head.word);
-				head = head.parentNode;
-			}
-			Collections.reverse(wordLadder);
-			return wordLadder;
-		}
-		
-		wordLadder.clear();
-		wordLadder.add(start);
-		wordLadder.add(end);
-		return wordLadder; 
+		ArrayList<String> emptylist = new ArrayList<String>();
+		emptylist.add(start.toLowerCase());
+		emptylist.add(end.toLowerCase());
+		return emptylist; // return empty ladder
+
 	}
-    
 
-	/**
-     * Locates all words in dictionary with a one letter difference between 
-     * them and the String; returns an ArrayList<Node> array 
-     * @param s : the String we are finding neighbors of
-     * @param dict : the Dictionary with available words
-     * @return	AArrayList<Node> array with all neighbor nodes
-     */
-	public static ArrayList<Node> allNeighbors(String s, Set<String> dict){
-    	ArrayList<Node> n = new ArrayList<Node>();
-    	
-    	for(int x = 0; x < s.length(); x++){
-    		for(char y = 'a'; y <= 'z'; y++){
-    			String temp = getWordToCheck(x, y, s);
-    			//Only adds words within the dictionary
-				if(dict.contains(temp.toUpperCase()) && !temp.equals(s) && oneLetterDifference(s, temp)){
-				
-						Node neigh = new Node(temp);
-						n.add(neigh);
-					
-					
-				}
-    		}
-    	}
-    	
-    	return n;
-    }
-
-
-	 
-
-	/**
-	 * This function takes a word index, a char, and a String and
-	 * replaces the String[word index] with the char
-	 * @param wordIndex is the index of the word to replace with char
-	 * @param alpha character
-	 * @param word	String
-	 * @return	A new string with the above changes
-	 */
-	public static String getWordToCheck(int wordIndex, char alpha, String word){
-    	word = word.replace(word.charAt(wordIndex), alpha);
-    	return word;
-    }
-    
-	public static Set<String>  makeDictionary () {
+	public static Set<String> makeDictionary() {
 		Set<String> words = new HashSet<String>();
 		Scanner infile = null;
 		try {
-			infile = new Scanner (new File("five_letter_words.txt"));
+			infile = new Scanner(new File("five_letter_words.txt"));
 		} catch (FileNotFoundException e) {
 			System.out.println("Dictionary File not Found!");
 			e.printStackTrace();
@@ -216,80 +193,67 @@ public class Main {
 		}
 		return words;
 	}
-    
-    public static ArrayList<String> DFS(String start, String end, ArrayList<String> visited, Set<String> Dictionary) {
-        
-        Dictionary.remove(start.toUpperCase());
-        
-        if (start.equals(end)) {
-            
-            
-            return visited;
-            
-        }
-        
-        for (int x = 0; x < start.length(); x++) {
-            StringBuilder startword = new StringBuilder(start);
-            for (int y = 0; y < 26; y++) {
-                
-                startword.setCharAt(x, (char) ('a' + y));
-                if (Dictionary.contains(startword.toString().toUpperCase()) && !visited.contains(startword.toString())) {
-                    
-                    visited.add(startword.toString());
-                    Dictionary.remove(startword.toString().toUpperCase());
-                    //					ArrayList<String> whatever = new ArrayList<String>();
-                    //					whatever = DFS(startword.toString(), end, visited, Dictionary);
-                    //					if(whatever != null){
-                    //						return whatever;
-                    //					}
-                    //					else{
-                    //						visited.remove(startword.toString());
-                    //					}
-                    ArrayList<String> whatever = new ArrayList<String>();
-                    whatever = DFS(startword.toString(), end, visited, Dictionary);
-                    if (whatever == null) {
-                        visited.remove(startword.toString());
-                        
-                    } else {
-                        //visited = DFS(startword.toString(), end, visited, Dictionary);
-                        return whatever;
-                    }
-                    
-                }
-                
-            }
-            
-        }
-        return null;
-        
-    }
-	
+/**
+ * 
+ * @param ladder
+ */
 	public static void printLadder(ArrayList<String> ladder) {
-        if(ladder == null){
-            System.out.print("no word ladder can be found between "+wordladder.get(0)+" and "+wordladder.get(1));
-            
-        }
-        System.out.print(ladder.toString());
-		
-	}
-	/**
-	 * This method determines whether there is a one letter
-	 * difference between String a and String b
-	 * @param a String
-	 * @param b	String
-	 * @return	true if one letter difference, false otherwise
-	 */
-	private static boolean oneLetterDifference(String a, String b){
-		int difference = 0;
-		for(int x = 0; x < a.length(); x++){
-			if(a.charAt(x) != b.charAt(x)){
-				difference++;
+		if (ladder.size() <=2 ) {
+			System.out.print("no word ladder can be found between " + ladder.get(0) + " and " + ladder.get(1));
+
+		} else {
+			System.out.println("a " + (ladder.size()) + "-rung word ladder exists between " + ladder.get(0) + " and "
+					+ ladder.get(ladder.size()-1));
+			for (String s : ladder) {
+				System.out.println(s);
 			}
 		}
-		//return true if the difference is <= 1
-		if(difference <= 1){
-			return true;
-		}
-		return false;
 	}
+	
+/**
+ * 
+ * @param start
+ * @param end
+ * @param visited
+ * @param Dictionary
+ * @return
+ */
+	public static ArrayList<String> DFS(String start, String end, ArrayList<String> visited, Set<String> Dictionary) {
+
+		Dictionary.remove(start.toUpperCase());
+		if (start.equals(end)) {
+			return visited;
+
+		}
+
+		for (int x = 0; x < start.length(); x++) {
+			StringBuilder startword = new StringBuilder(start);
+			for (int y = 0; y < 26; y++) {
+
+				startword.setCharAt(x, (char) ('a' + y));
+				if (Dictionary.contains(startword.toString().toUpperCase())
+						&& !visited.contains(startword.toString())) {
+
+					visited.add(startword.toString());
+					Dictionary.remove(startword.toString().toUpperCase());
+
+					ArrayList<String> visitladder = new ArrayList<String>();
+					visitladder = DFS(startword.toString(), end, visited, Dictionary);
+					if (visitladder == null) {
+						visited.remove(startword.toString());
+
+					} else {
+						
+						return visitladder;
+					}
+
+				}
+
+			}
+
+		}
+		return null;
+
+	}
+
 }
--- project3/src/assignment3/Node.java
+++ project3/src/assignment3/Node.java
@@ -1,12 +1,26 @@
-package assignment3;
+/* WORD LADDER Main.java
+ * EE422C Project 3 submission by
+ * Replace <...> with your actual data.
+ * <Student1 Name>
+ * <Student1 EID>
+ * <Student1 5-digit Unique No.>
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Git URL:
+ * Fall 2016
+ */
 
-import java.util.ArrayList;
+package project3;
+
+import java.util.*;
 
 public class Node {
-	public Node parentNode = null;
-	public String word;
-	public ArrayList<Node> neighbors = new ArrayList<Node>();
-	Node(String s){
-		this.word = s;
-	}
+
+
+ArrayList<String> storeladder = new ArrayList<String>();
+String storeword;
+
+
 }
